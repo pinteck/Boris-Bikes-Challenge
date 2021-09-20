5fbaba5fc678b8ed8d32f1849253f409df5d426e
pinar <pnr_ozbakir@msn.com>
2021-09-20 20:54:04 +0100

    add DockingStation class and define release_bike method inside the class

---
+++ lib/docking_station.rb
@@ -0,0 +1,7 @@
+class DockingStation
+  def release_bike
+  end
+
+  def self.working
+  end
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+require './lib/docking_station.rb'
+
+describe DockingStation do
+  it 'releases bike' do
+    expect(DockingStation.new.release_bike).to respond_to
+  end
+
+  it 'calls a static method' do
+    expect(DockingStation.working).to respond_to
+  end  
+ end
