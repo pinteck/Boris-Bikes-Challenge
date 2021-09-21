de240b4f61babdc9241e7e165c7ed7da8eb5727f
pinar <pnr_ozbakir@msn.com>
2021-09-21 15:41:57 +0100

    Complete Challenge 9

---
+++ spec/bike_spec.rb
@@ -0,0 +1,5 @@
+describe Bike do
+  it "checks if the bike is working" do
+  expect(subject.working?) respond_to?
+  end
+end 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,14 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
   it 'releases bike' do
-    expect(DockingStation.new.release_bike).to respond_to
+    expect(DockingStation.new.release_bike).to respond_to?
   end
 
-  it 'calls a static method' do
-    expect(DockingStation.working).to respond_to
-  end  
+  # it 'calls a static method' do
+  #   expect(DockingStation.working).to respond_to
+  # end  
  end
+
+describe Bike do
+  
+end 
