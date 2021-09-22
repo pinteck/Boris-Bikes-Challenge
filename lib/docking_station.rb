a57c9d304a99cc8ff9fd427564ee62ec139e7249
pinar <pnr_ozbakir@msn.com>
2021-09-22 19:52:23 +0100

    complete chapter 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,17 +3,18 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
-  def release_bike
-    Bike.new
-  end
+  # def release_bike
+  #   Bike.new
+  # end
 
   def dock(bike)
     @bike = bike
   end
 
-  # def release_bike
-  #   @bike ? @bike : raise
-  # end
+  def release_bike
+    fail 'No bikes available' if not @bike
+    @bike
+  end
 
   # def bike
   #   @bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,23 +2,40 @@ require './lib/docking_station.rb'
 
 # one-liner code 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike} 
+  # previous code
+  # it {is_expected.to respond_to :release_bike} 
 
-  it 'releases a working bike' do
-    expect(DockingStation.new.release_bike).to be_working
+  # it 'releases a working bike' do
+  #   expect(DockingStation.new.release_bike).to be_working
+  # end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
   end
 
-  it "returns docked bikes" do 
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end 
   end
+
+  # previous code 
+  # it "returns docked bikes" do 
+  #   bike = Bike.new
+  #   subject.dock(bike)
+  #   expect(subject.bike).to eq bike
+  # end
 
   it { is_expected.to respond_to(:bike)}
 
-  # it "raises an error" do
-  #   expect {subject.bike_avaible?}.to raise_error(NoMethodError)
-  # end
+  it "raises an error" do
+    expect {subject.bike_avaible?}.to raise_error(NoMethodError)
+  end
 end
 
 
