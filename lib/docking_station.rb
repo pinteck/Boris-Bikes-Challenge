552225aedee07479d75376ad57546ded3aafede8
pinar <pnr_ozbakir@msn.com>
2021-09-22 20:10:43 +0100

    complete chapter 13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,17 @@ class DockingStation
   # end
 
   def dock(bike)
+    fail 'The docking station is full' if @bike
     @bike = bike
   end
 
   def release_bike
     fail 'No bikes available' if not @bike
     @bike
+  end
+
+  def release_bike
+    
   end
 
   # def bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,19 +9,26 @@ describe DockingStation do
   #   expect(DockingStation.new.release_bike).to be_working
   # end
 
-  describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      # we want to release the bike we docked
-      expect(subject.release_bike).to eq bike
-    end
-  end
+  # describe '#release_bike' do
+  #   it 'releases a bike' do
+  #     bike = Bike.new
+  #     subject.dock(bike)
+  #     # we want to release the bike we docked
+  #     expect(subject.release_bike).to eq bike
+  #   end
+  # end
 
-  describe '#release_bike' do
-    it 'raises an error when there are no bikes available' do
-      expect { subject.release_bike }.to raise_error 'No bikes available'
-    end 
+  # describe '#release_bike' do
+  #   it 'raises an error when there are no bikes available' do
+  #     expect { subject.release_bike }.to raise_error 'No bikes available'
+  #   end 
+  # end
+
+  describe '#dock' do
+    it 'raises and error when the capacity is full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'The docking station is full'
+    end
   end
 
   # previous code 
